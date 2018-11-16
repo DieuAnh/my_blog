3e5e2b842065657338b02b3bdfa9787e39e84e3a
Dieu Anh <anhlee2101@gmail.com>
2018-11-16 11:14:16 +0100

    add localhost

--- my_django_blogging_app/settings.py
+++ my_django_blogging_app/settings.py
@@ -138,4 +138,4 @@ import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
-ALLOWED_HOSTS = ['heroku-da-s-blog.herokuapp.com']
+ALLOWED_HOSTS = ['heroku-da-s-blog.herokuapp.com', '127.0.0.1:8000']
