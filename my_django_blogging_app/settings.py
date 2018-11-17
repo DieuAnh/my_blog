7373eca18f3e168414f1be155dd69c523807322d
Dieu Anh <anhlee2101@gmail.com>
2018-11-17 09:54:03 +0100

    update settings.py

--- my_django_blogging_app/settings.py
+++ my_django_blogging_app/settings.py
@@ -124,11 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 # PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/templates/'
+STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'templates'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 #  Add configuration for static files storage using whitenoise
