26a47d007435054a11bbf99aa69cea4ee1c034f2
Dieu Anh <anhlee2101@gmail.com>
2018-12-08 17:08:07 +0100

    add feature to approve commnets on posts

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,9 @@ class Post(models.Model):
         seft.published_date = timezone.now()
         seft.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved=True)
+
     def __str__(seft):
         return seft.title
 
@@ -21,6 +24,11 @@ class Comment(models.Model):
     author = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
+    approved = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved = True
+        self.save()
 
     def __str__(self):
         return self.text
