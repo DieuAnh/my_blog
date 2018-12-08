1431daf24a4da0064fdb3be25165d6968b9af97b
Dieu Anh <anhlee2101@gmail.com>
2018-12-08 11:26:27 +0100

    add logout functionality

--- manage.py
+++ manage.py
@@ -6,10 +6,17 @@ if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_django_blogging_app.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+    except ImportError:
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid making other
+        # exception on Python2
+        try:
+            import django
+        except ImportError:
+            raise ImportError(
+                    "Couldn't import Django. Are you sure it's installed and "
+                    "available on your PYTHONPATH environment variable? Did you "
+                    "forget to activate a virtual environment?"
+            )
+        raise
     execute_from_command_line(sys.argv)
